59469eafc420c6182ecf63dc41f8d5ab54c155b3
Jordan Nishina <jordannishina2021@u.northwestern.edu>
2020-02-19 20:42:18 -0600

    commented for commit

--- src/PatientComponents/Patient.js
+++ src/PatientComponents/Patient.js
@@ -8,6 +8,8 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+//commenting for commit
+
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
