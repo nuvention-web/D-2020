def896a0837f615301755a1ce97b6998b28c1e4f
Jordan Nishina <jordannishina2021@u.northwestern.edu>
2020-02-25 15:27:23 -0600

    implemented carousel and timer for exercise tracking

--- src/App.js
+++ src/App.js
@@ -32,9 +32,12 @@ const App = () => {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/workout">
+        <Route path="/workout">
             <ExerciseTracking></ExerciseTracking>
           </Route>
+          {/* <Route path="/workout">
+            <PatientExerciseMain></PatientExerciseMain>
+          </Route> */}
           {/* <Route path="/users">
             <Users />
           </Route> */}
